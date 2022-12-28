93a8856203be8bc3061f9b562f08992acff47ddf
nhintaoak <92565914+nhintaoak@users.noreply.github.com>
2022-12-28 10:29:08 +0700

    Add project files.

---
+++ CapstoneProject-BE/Constants/Constant.cs
@@ -0,0 +1,9 @@
+﻿using System.Runtime.ConstrainedExecution;
+
+namespace CapstoneProject_BE.Constants
+{
+    public class Constant
+    {
+        public static readonly int Constant0 = 1;
+    }
+}
---
+++ CapstoneProject-BE/Controllers/Admin/ExamplesController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CapstoneProject_BE.Controllers.Admin
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ExamplesController : ControllerBase
+    {
+        [HttpGet]
+        public string GetConfirmationToken(string UserId)
+        {
+            return UserId;
+        }
+    }
+}
