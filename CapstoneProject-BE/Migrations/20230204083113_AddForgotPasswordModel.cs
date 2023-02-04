0648fd77f63bab90c0e4fa8ecc62357c2c80b2a8
Trung Kien <kiennthe153296@fpt.edu.vn>
2023-02-04 16:30:08 +0700

    forgot password and send email
    initial code

---
+++ CapstoneProject-BE/Migrations/20230204083113_AddForgotPasswordModel.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace CapstoneProject_BE.Migrations
+{
+    public partial class AddForgotPasswordModel : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "ForgotPasswordModel",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Email = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    EmailSent = table.Column<bool>(type: "bit", nullable: false),
+                    Token = table.Column<string>(type: "nvarchar(max)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_ForgotPasswordModel", x => x.Id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "ForgotPasswordModel");
+        }
+    }
+}
--- CapstoneProject-BE/Models/InventoryManagementContext.cs
+++ CapstoneProject-BE/Models/InventoryManagementContext.cs
@@ -4,24 +4,25 @@ namespace CapstoneProject_BE.Models
 {
     public class InventoryManagementContext : DbContext
     {
-        public InventoryManagementContext(DbContextOptions<InventoryManagementContext> options): base(options)
-       {
+        public InventoryManagementContext(DbContextOptions<InventoryManagementContext> options) : base(options)
+        {
 
-       }
+        }
         public DbSet<User> Users { get; set; }
         public DbSet<Role> Roles { get; set; }
         public DbSet<RefreshToken> RefreshTokens { get; set; }
+        public DbSet<ForgotPasswordModel> ForgotPasswordModels { get; set; }
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
             modelBuilder.Entity<User>(e =>
             {
                 e.ToTable("User");
-                e.HasKey(u=>u.UserId);
+                e.HasKey(u => u.UserId);
                 e.HasOne(u => u.Role)
-                .WithMany(u=>u.Users)
-                .HasForeignKey(u=>u.RoleId);
+                .WithMany(u => u.Users)
+                .HasForeignKey(u => u.RoleId);
                 e.Property(u => u.UserName).IsRequired();
-                e.Property(u=>u.Password).IsRequired();
+                e.Property(u => u.Password).IsRequired();
                 e.Property(u => u.RoleId).HasDefaultValue(0);
                 e.Property(u => u.UserId).UseIdentityColumn();
             });
@@ -32,18 +33,24 @@ namespace CapstoneProject_BE.Models
                 e.Property(r => r.RoleName).IsRequired();
                 e.Property(u => u.RoleId).UseIdentityColumn();
             });
-            modelBuilder.Entity<RefreshToken>(e=>
+            modelBuilder.Entity<RefreshToken>(e =>
             {
                 e.ToTable("RefreshToken");
                 e.HasKey("TokenId");
                 e.HasOne(rf => rf.User)
                 .WithOne(u => u.RefreshToken)
-                .HasForeignKey<RefreshToken>(u=>u.UserId);
+                .HasForeignKey<RefreshToken>(u => u.UserId);
                 e.Property(u => u.IsRevoked).HasDefaultValue(false);
                 e.Property(u => u.TokenId).UseIdentityColumn();
-            }
-                );
-
+            });
+            modelBuilder.Entity<ForgotPasswordModel>(e =>
+            {
+                e.ToTable("ForgotPasswordModel");
+                e.HasKey(r => r.Id);
+                e.Property(r => r.Email).IsRequired();
+                e.Property(r => r.EmailSent).IsRequired();
+                e.Property(u => u.Token).IsRequired();
+            });
         }
     }
 }
