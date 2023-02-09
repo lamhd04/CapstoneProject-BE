dc892f2a7728c30a42c0b0b46da0fd64daf8efae
nhintaoak <92565914+nhintaoak@users.noreply.github.com>
2023-02-09 09:05:13 +0700

    update product

--- CapstoneProject-BE/DTO/ProductDTO.cs
+++ CapstoneProject-BE/DTO/ProductDTO.cs
@@ -5,18 +5,18 @@ namespace CapstoneProject_BE.DTO
     public class ProductDTO
     {
         public int ProductId { get; set; }
-        public string ProductName { get; set; }
-        public string ProductCode { get; set; }
+        public string? ProductName { get; set; }
+        public string? ProductCode { get; set; }
         public int CategoryId { get; set; }
-        public string Description { get; set; }
+        public string? Description { get; set; }
         public int SupplierId { get; set; }
-        public float CostPrice { get; set; }
-        public float SellingPrice { get; set; }
-        public string DefaultMeasuredUnit { get; set; }
-        public int InStock { get; set; }
-        public float StockPrice { get; set; }
-        public string Image { get; set; }
-        public string Status { get; set; }
-        public List<MeasuredUnitDTO> MeasuredUnits { get; set; }
+        public float? CostPrice { get; set; }
+        public float? SellingPrice { get; set; }
+        public string? DefaultMeasuredUnit { get; set; }
+        public int? InStock { get; set; }
+        public float? StockPrice { get; set; }
+        public string? Image { get; set; }
+        public bool? Status { get; set; }
+        public List<MeasuredUnitDTO>? MeasuredUnits { get; set; }
     }
 }
