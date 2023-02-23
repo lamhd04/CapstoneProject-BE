eda0405ec83d540d53a06806eb0165665874e210
Trung Kien <kiennthe153296@fpt.edu.vn>
2023-02-24 04:09:47 +0700

    define entity export

---
+++ CapstoneProject-BE/Models/ExportOrder.cs
@@ -0,0 +1,24 @@
+﻿namespace CapstoneProject_BE.Models
+{
+    public class ExportOrder
+    {
+        public int ExportId { get; set; }
+        public string ExportCode { get; set; }
+        public int UserId { get; set; }
+        public int TotalAmount { get; set; }
+        public float Total { get; set; }
+        public float TotalCost { get; set; }
+        public string? Note { get; set; }
+        public int State { get; set; }
+        public float Paid { get; set; }
+        public float InDebted { get; set; }
+        public float OtherExpense { get; set; }
+        public DateTime Created { get; set; }
+        public DateTime? Approved { get; set; }
+        public DateTime? Completed { get; set; }
+        public DateTime? Denied { get; set; }
+        public virtual User User { get; set; }
+        public virtual ICollection<ExportOrderDetail> ExportOrderDetails { get; set; }
+        public virtual Supplier Supplier { get; set; }
+    }
+}
