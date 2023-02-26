1039ca4492cf59bb7c588339660f0d8536ddc9f9
nhintaoak <92565914+nhintaoak@users.noreply.github.com>
2023-02-26 22:16:31 +0700

    update export

---
+++ CapstoneProject-BE/Migrations/20230226151524_updateExportv3.cs
@@ -0,0 +1,48 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace CapstoneProject_BE.Migrations
+{
+    public partial class updateExportv3 : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "InDebted",
+                table: "ExportOrder");
+
+            migrationBuilder.DropColumn(
+                name: "OtherExpense",
+                table: "ExportOrder");
+
+            migrationBuilder.DropColumn(
+                name: "Paid",
+                table: "ExportOrder");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<float>(
+                name: "InDebted",
+                table: "ExportOrder",
+                type: "real",
+                nullable: false,
+                defaultValue: 0f);
+
+            migrationBuilder.AddColumn<float>(
+                name: "OtherExpense",
+                table: "ExportOrder",
+                type: "real",
+                nullable: false,
+                defaultValue: 0f);
+
+            migrationBuilder.AddColumn<float>(
+                name: "Paid",
+                table: "ExportOrder",
+                type: "real",
+                nullable: false,
+                defaultValue: 0f);
+        }
+    }
+}
