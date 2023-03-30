08b08d803427a2a95e8561c4670081e83d33acdb
nhintaoak <92565914+nhintaoak@users.noreply.github.com>
2023-03-30 21:19:14 +0700

    update cate

--- CapstoneProject-BE/DTO/CategoryDTO.cs
+++ CapstoneProject-BE/DTO/CategoryDTO.cs
@@ -2,8 +2,8 @@
 {
     public class CategoryDTO
     {
-        public int CategoryId { get; set; }
+        public int? CategoryId { get; set; }
         public string CategoryName { get; set; }
-        public string Description { get; set; }
+        public string? Description { get; set; }
     }
 }
--- CapstoneProject-BE/Models/Category.cs
+++ CapstoneProject-BE/Models/Category.cs
@@ -6,7 +6,7 @@ namespace CapstoneProject_BE.Models
     {
         public int CategoryId { get; set; }
         public string CategoryName { get; set; }
-        public string Description { get; set; } 
+        public string? Description { get; set; } 
         [JsonIgnore]
         public ICollection<Product>? Products { get; set; }
         public int StorageId { get; set; }
